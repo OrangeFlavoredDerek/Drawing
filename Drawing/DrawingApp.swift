6fc6c11db16d55048a35c22762abc5d898d43d58
Derek <chenjunqi07@icloud.com>
2020-09-27 16:26:28 +0800

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Derek Chan on 2020/9/27.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
